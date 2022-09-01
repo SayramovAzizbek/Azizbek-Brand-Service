5ccfd3c9ad4ed7dfb1f0f8c0fc801e37463de835
SayramovAzizbek <sayramovazizbek@gmail.com>
2022-09-01 13:47:16 +0500

    Finish Header Section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* Start Header Navbar */
+const siteHeaderBtn = document.querySelector(".header-part__btn")
+const siteHeader = document.querySelector("#site-header")
+
+siteHeaderBtn.addEventListener("click", () => {
+    siteHeader.classList.toggle("header-part__nav-box-on")
+})
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth < 700) resize.classList.remove("header-part__nav-box-on");
+});
+/* End of Header Navbar */
